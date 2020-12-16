ac7bfed2849d6b0ce374b2435068912c4aab9e34
startec-official <startec.innovations@gmail.com>
2020-12-16 14:02:18 +0800

    fixed table not creating issue

--- mysql.js
+++ mysql.js
@@ -11,19 +11,20 @@ var preConnect = mysql.createConnection({
 
 var preCheck = function() {
   const createTableQuery = `
-  CREATE  TABLE IF NOT EXISTS ${process.env.database}.users (
+  CREATE TABLE IF NOT EXISTS ${process.env.database}.users (
     name VARCHAR(255) NOT NULL,
     age VARCHAR(50) NOT NULL,
     birthdate VARCHAR(50) NOT NULL,
     sex VARCHAR(50) NOT NULL,
     address VARCHAR(255) NOT NULL,
     phone_number VARCHAR(50) NOT NULL,
-    datetime VARCHAR(255)) NOT NULL
+    datetime VARCHAR(255) NOT NULL
+    )
   ENGINE = InnoDB;
   `;
   preConnect.query( `CREATE DATABASE IF NOT EXISTS ${ process.env.database };` , ( err , rows , fields ) => {
     if( err ) throw err;
-    preConnect.query( createTableQuery , ( err , rows , fields ) => {
+    connection.query( createTableQuery , ( err , rows , fields ) => {
       if( err ) return err;
       console.log( rows );
     });
