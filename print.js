ea4d77596454c5f0855fa83ba443029c72408ecc
startec-official <startec.innovations@gmail.com>
2020-12-20 07:10:12 +0800

    test

--- print.js
+++ print.js
@@ -5,28 +5,29 @@ var bonjour = require('bonjour')();
 
 var testPrint = () => {
     var doc = new PDFDocument({margin:0});
-doc.text(".", 0, 0);
+    doc.text(".", 0, 0);
 
-doc.pipe(concat(function (data) {
-	var printer = ipp.Printer("http://localhost:631/ipp/printer");
-	var msg = {
-		"operation-attributes-tag": {
-			"requesting-user-name": "Bumblebee",
-			"job-name": "whatever.pdf",
-			"document-format": "application/pdf"
-		},
-		"job-attributes-tag":{
-      "media-col": {
-        "media-source": "tray-2"
-      }
-		}
-		, data: data
-	};
-	printer.execute("Print-Job", msg, function(err, res){
-		console.log(err);
-		console.log(res);
-	});
-}));
+    doc.pipe(concat(function (data) {
+        var printer = ipp.Printer("http://localhost:631/ipp/printer");
+        var msg = {
+            "operation-attributes-tag": {
+                "requesting-user-name": "Bumblebee",
+                "job-name": "whatever.pdf",
+                "document-format": "application/pdf"
+            },
+            "job-attributes-tag":{
+        "media-col": {
+            "media-source": "tray-2"
+        }
+            }
+            , data: data
+        };
+        printer.execute("Print-Job", msg, function(err, res){
+            console.log(err);
+            console.log(res);
+        });
+    }));
+    doc.end();
 }
 
 var findPrinters = () => {
@@ -36,5 +37,3 @@ var findPrinters = () => {
 }
 
 findPrinters();
-
-doc.end();
