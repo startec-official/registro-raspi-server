974ac2fff80ecad907899274dc6c024fd54c87e4
startec-official <startec.innovations@gmail.com>
2020-12-20 06:47:28 +0800

    test 2

--- print.js
+++ print.js
@@ -1,21 +1,29 @@
 var ipp = require('ipp');
 var PDFDocument = require('pdfkit');
- 
-//make a PDF document
+var concat = require("concat-stream");
+
 var doc = new PDFDocument({margin:0});
-doc.text(".", 0, 780);
- 
-doc.output(function(pdf){
-    var printer = ipp.Printer("http://localhost/ipp/printer");
-    var msg = {
-        "operation-attributes-tag": {
-            "requesting-user-name": "Startec",
-            "job-name": "My Test Job",
-            "document-format": "application/pdf"
-        },
-        data: pdf
-    };
-    printer.execute("Print-Job", msg, function(err, res) {
-        console.log(res);
-    });
-});
+doc.text(".", 0, 0);
+
+
+doc.pipe(concat(function (data) {
+	var printer = ipp.Printer("http://localhost:631/ipp/printer");
+	var msg = {
+		"operation-attributes-tag": {
+			"requesting-user-name": "Bumblebee",
+			"job-name": "whatever.pdf",
+			"document-format": "application/pdf"
+		},
+		"job-attributes-tag":{
+      "media-col": {
+        "media-source": "tray-2"
+      }
+		}
+		, data: data
+	};
+	printer.execute("Print-Job", msg, function(err, res){
+		console.log(err);
+		console.log(res);
+	});
+}));
+doc.end();
